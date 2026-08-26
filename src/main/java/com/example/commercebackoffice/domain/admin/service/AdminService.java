233eee67ae7082b7fdadc493d4cae5dd2c45b768
chungmani <cndaks19@naver.com>
2026-08-27 02:13:13 +0900

    feat: 비밀번호 암호화 기능 구현

--- src/main/java/com/example/commercebackoffice/domain/admin/service/AdminService.java
+++ src/main/java/com/example/commercebackoffice/domain/admin/service/AdminService.java
@@ -1,5 +1,6 @@
 package com.example.commercebackoffice.domain.admin.service;
 
+import com.example.commercebackoffice.common.config.PasswordEncoder;
 import com.example.commercebackoffice.domain.admin.dto.CreateAdminRequest;
 import com.example.commercebackoffice.domain.admin.dto.CreateAdminResponse;
 import com.example.commercebackoffice.domain.admin.entity.Admin;
@@ -14,10 +15,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class AdminService {
 
     private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
 
     // 회원가입
     @Transactional
     public CreateAdminResponse create(CreateAdminRequest request) {
+
+        boolean existEmail = adminRepository.existsByEmail(request.email());
+        if (!existEmail) {
+            throw new IllegalStateException("이미 가입한 이메일입니다.");
+        }
+
+        // 비밀번호 암호화
+        String passwordHashed = passwordEncoder.encode(request.password());
+
         Admin admin = new Admin(
                 request.name(), request.email(), request.password(),
                 request.phoneNumber(), request.role()
