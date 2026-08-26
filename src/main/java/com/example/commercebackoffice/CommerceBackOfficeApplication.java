5dc6295c7571fa93c20358abf0552aa70c350cec
chungmani <cndaks19@naver.com>
2026-08-26 15:42:26 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/example/commercebackoffice/CommerceBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.commercebackoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceBackOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceBackOfficeApplication.class, args);
+    }
+
+}
