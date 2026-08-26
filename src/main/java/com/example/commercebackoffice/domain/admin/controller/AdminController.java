3888e170395dd26491e8531dd71a850aa8fdc371
chungmani <cndaks19@naver.com>
2026-08-27 01:59:59 +0900

    feat: Admin 회원가입 기능 구현

--- src/main/java/com/example/commercebackoffice/domain/admin/controller/AdminController.java
+++ src/main/java/com/example/commercebackoffice/domain/admin/controller/AdminController.java
@@ -1,12 +1,32 @@
 package com.example.commercebackoffice.domain.admin.controller;
 
+import com.example.commercebackoffice.domain.admin.dto.CreateAdminRequest;
+import com.example.commercebackoffice.domain.admin.dto.CreateAdminResponse;
+import com.example.commercebackoffice.domain.admin.enums.AdminRole;
 import com.example.commercebackoffice.domain.admin.service.AdminService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/admins")
 public class AdminController {
 
     private final AdminService adminService;
+
+    // 회원가입
+    @PostMapping("/signup")
+    public ResponseEntity<CreateAdminResponse> create(@RequestBody CreateAdminRequest request) {
+        if (request.equals(AdminRole.SUPER_ADMIN)) {
+            throw new IllegalStateException("슈퍼관리자로 가입할 수 없습니다.");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(adminService.create(request));
+    }
+
+
 }
--- src/main/java/com/example/commercebackoffice/domain/admin/service/AdminService.java
+++ src/main/java/com/example/commercebackoffice/domain/admin/service/AdminService.java
@@ -1,13 +1,29 @@
 package com.example.commercebackoffice.domain.admin.service;
 
+import com.example.commercebackoffice.domain.admin.dto.CreateAdminRequest;
+import com.example.commercebackoffice.domain.admin.dto.CreateAdminResponse;
+import com.example.commercebackoffice.domain.admin.entity.Admin;
 import com.example.commercebackoffice.domain.admin.repository.AdminRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AdminService {
 
     private final AdminRepository adminRepository;
 
+    // 회원가입
+    @Transactional
+    public CreateAdminResponse create(CreateAdminRequest request) {
+        Admin admin = new Admin(
+                request.name(), request.email(), request.password(),
+                request.phoneNumber(), request.role()
+        );
+
+        Admin savedAdmin = adminRepository.save(admin);
+        return CreateAdminResponse.from(savedAdmin);
+    }
 }
