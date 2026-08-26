8d66d611dd9f3f962cf1f690ce20cce8bde97053
chungmani <cndaks19@naver.com>
2026-08-26 22:56:11 +0900

    feat: Admin 엔티티 생성, 역할, 상태 enum 생성

---
+++ src/main/java/com/example/commercebackoffice/admin/enums/AdminRole.java
@@ -0,0 +1,11 @@
+package com.example.commercebackoffice.admin.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum AdminRole {
+    SUPER_ADMIN,
+    OPERATION_ADMIN,
+    MARKETING_ADMIN,
+    CUSTOMER_SUPPORT_ADMIN
+}
---
+++ src/main/java/com/example/commercebackoffice/admin/enums/AdminState.java
@@ -0,0 +1,12 @@
+package com.example.commercebackoffice.admin.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum AdminState {
+    ACTIVE,
+    INACTIVE,
+    SUSPENDED,
+    PENDING,
+    DENIED
+}
