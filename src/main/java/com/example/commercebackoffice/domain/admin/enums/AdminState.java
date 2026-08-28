a0cbd57c48887d4dc41728b26f4eb8dfc31cc272
chungmani <cndaks19@naver.com>
2026-08-28 22:26:08 +0900

    feat: 전역 예외처리 및 AdminState 리팩토링

--- src/main/java/com/example/commercebackoffice/domain/admin/enums/AdminState.java
+++ src/main/java/com/example/commercebackoffice/domain/admin/enums/AdminState.java
@@ -4,9 +4,18 @@ import lombok.Getter;
 
 @Getter
 public enum AdminState {
-    ACTIVE,
-    INACTIVE,
-    SUSPENDED,
-    PENDING,
-    DENIED
+    ACTIVE(""),
+    INACTIVE("비활성화된 계정입니다."),
+    SUSPENDED("정지된 계정입니다."),
+    PENDING("승인대기중인 계정입니다."),
+    DENIED("거부된 계정입니다.");
+
+    String message;
+    AdminState(String message) {
+        this.message = message;
+    }
+
+    public boolean canLogin() {
+        return this == ACTIVE;
+    }
 }
