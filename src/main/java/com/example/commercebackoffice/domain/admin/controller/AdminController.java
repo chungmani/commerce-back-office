43c72d1fe85f3d9c610c1568fabc49de2dca5c2d
chungmani <cndaks19@naver.com>
2026-08-28 23:17:15 +0900

    feat: 로그아웃 기능 구현

--- src/main/java/com/example/commercebackoffice/domain/admin/controller/AdminController.java
+++ src/main/java/com/example/commercebackoffice/domain/admin/controller/AdminController.java
@@ -8,10 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,4 +33,17 @@ public class AdminController {
         return ResponseEntity.ok(LoginResponse.from(admin));
     }
 
+    // 로그아웃
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(
+            @SessionAttribute(name = "loginAdmin", required = false)
+            SessionAdmin sessionAdmin, HttpSession session
+    ) {
+        if (sessionAdmin == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        session.invalidate();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
