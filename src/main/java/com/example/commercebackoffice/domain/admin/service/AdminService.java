38c1851c38de4213fa0b9c04b2817098a9847c36
chungmani <cndaks19@naver.com>
2026-09-05 01:24:05 +0900

    feat: 관리자 리스트 조회

--- src/main/java/com/example/commercebackoffice/domain/admin/service/AdminService.java
+++ src/main/java/com/example/commercebackoffice/domain/admin/service/AdminService.java
@@ -20,9 +20,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
